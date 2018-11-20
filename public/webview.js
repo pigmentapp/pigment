35e258eafbac7f568552056f24bb0026e6c1fbd2
herteleo <leonard.hertel@gmail.com>
2018-11-20 22:04:31 +0100

    resolve relative notification icon path

--- public/webview.js
+++ public/webview.js
@@ -1,11 +1,18 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { ipcRenderer } = require('electron');
+const isUrlRelativeUrl = require('is-url-relative-without-domain');
 
 const OriginalNotification = Notification;
 
 // eslint-disable-next-line func-names
 window.Notification = function (title, options) {
-  ipcRenderer.sendToHost('notification', { title, options });
+  const notificationOptions = options;
+
+  if (notificationOptions.icon && isUrlRelativeUrl(notificationOptions.icon)) {
+    notificationOptions.icon = `${window.location.origin}/${notificationOptions.icon}`;
+  }
+
+  ipcRenderer.sendToHost('notification', { title, notificationOptions });
 };
 
 Notification.permission = OriginalNotification.permission;
