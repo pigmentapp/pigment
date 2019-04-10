f8a39a3ced904f456ebd00fe4db3c43bbf0720ce
herteleo <leonard.hertel@gmail.com>
2019-04-10 19:43:21 +0200

    notification styling

--- src/views/Notifications.vue
+++ src/views/Notifications.vue
@@ -16,20 +16,44 @@
       <div
         v-for="item in notifications"
         :key="item.timestamp"
-        class="p-2 mb-2 bg-black rounded"
+        class="flex p-2 mb-2 bg-black rounded"
       >
-        <div class="flex mb-1">
-          <div class="flex-grow font-bold">{{ item.notification.title }}</div>
-          <div class="mx-2">{{ new Date(item.timestamp).toLocaleString() }}</div>
-          <button
-            type="button"
-            class="text-grey-dark"
-            @click="removeItem(item)"
+        <div
+          class="
+            bg-grey-lightest
+            flex flex-no-shrink justify-center items-center
+            p-1 w-8 h-8 mr-3 rounded-sm
+          "
+        >
+          <img
+            v-if="item.notification.options && item.notification.options.icon"
+            :src="item.notification.options.icon"
+            class="block max-w-full max-h-full"
           >
-            &times;
-          </button>
+          <app-icon
+            v-else
+            face="tab"
+          />
         </div>
-        <div>{{ item.notification.options.body }}</div>
+        <div class="flex-grow">
+          <div class="flex mb-1">
+            <div class="flex-grow font-bold">{{ item.notification.title }}</div>
+            <div class="mx-2 text-grey-dark">{{ new Date(item.timestamp).toLocaleString() }}</div>
+            <button
+              type="button"
+              class="text-grey-dark"
+              @click="removeItem(item)"
+            >
+              &times;
+            </button>
+          </div>
+          <div
+            v-if="item.notification.options"
+            class="mt-2 text-grey-dark"
+          >
+            {{ item.notification.options.body }}
+          </div>
+        </div>
       </div>
     </template>
     <div
