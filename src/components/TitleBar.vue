151bca43284d0f115d237b0b0c6b019eeb067266
herteleo <leonard.hertel@gmail.com>
2019-04-17 01:02:34 +0200

    feat: general title bar and buttons

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    :class="{
+      [$style.bar]: true,
+      [$style.indent]: $options.isMac && indent
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  isMac: process.platform === 'darwin',
+  props: {
+    indent: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="postcss" module>
+.bar {
+  @apply flex items-center px-1 leading-none;
+  height: 2.4rem;
+  background-color: #0009;
+}
+
+.indent {
+  @apply pl-20;
+}
+</style>
---
+++ src/components/TitleBarButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+    :class="{
+      [$style.btn]: true,
+      [$style.active]: active,
+    }"
+    type="button"
+    @click="$emit('click')"
+  >
+    <app-icon :face="icon" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="postcss" module>
+.btn {
+  @apply p-2 leading-none appearance-none text-grey-dark;
+}
+
+.btn:hover {
+  @apply text-blue-lighter bg-blue;
+}
+
+.active {
+  @apply text-blue-lighter bg-blue;
+}
+</style>
