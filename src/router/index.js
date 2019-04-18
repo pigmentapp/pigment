62e0a74fa7fe9dfdb299bd82862f36ef337fa029
herteleo <leonard.hertel@gmail.com>
2019-04-19 00:47:00 +0200

    fix(routing): removed unused routes

--- src/router/index.js
+++ src/router/index.js
@@ -24,13 +24,5 @@ export default new Router({
       name: 'settings',
       component: () => import(/* webpackChunkName: "settings" */ '@/views/Settings.vue'),
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
-    },
   ],
 });
