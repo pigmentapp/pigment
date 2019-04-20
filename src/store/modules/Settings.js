b42e1714f08445dfcec6a188b84df64d81ff40fb
herteleo <leonard.hertel@gmail.com>
2019-04-20 22:44:50 +0200

    feat(settings): new structure and saved as settings.json

--- src/store/modules/Settings.js
+++ src/store/modules/Settings.js
@@ -1,11 +1,33 @@
+import Connection from '@/db';
+
+const db = new Connection({
+  file: 'settings.json',
+  defaults: {
+    'dimmer.dimDelayInMs': 5000,
+    'dimmer.dimIfWindowIsNotInFocus': false,
+    'layout.sideBarLocation': 'left',
+    'notifications.holdBackIfWindowIsNotInFocus': false,
+    'notifications.sendSummaryIfWindowIsNotInFocus': false,
+    'notifications.summaryIntervalInMs': 1200000,
+    'window.muteAudioIfWindowIsNotInFocus': false,
+  },
+});
+
 export default {
   namespaced: true,
   state: {
     isDimActive: false,
     isLayoutInverted: false,
     muteOnWindowBlur: false,
+    dbUpdated: Date.now(),
   },
   getters: {
+    all({ dbUpdated }) {
+      return db(dbUpdated).value();
+    },
+    byKey({ dbUpdated }) {
+      return key => db(dbUpdated).get(key).value();
+    },
     isDimActive({ isDimActive }) {
       return isDimActive;
     },
@@ -26,7 +48,14 @@ export default {
     setMuteOnWindowBlur(state, yesNo) {
       state.muteOnWindowBlur = yesNo;
     },
+    triggerDbUpdate(state) {
+      state.dbUpdated = Date.now();
+    },
   },
   actions: {
+    set({ commit }, [key, value]) {
+      db().set(key, value).write();
+      commit('triggerDbUpdate');
+    },
   },
 };
