e5d2eb97f337caf7b4caed5614e99ae23baa3ba1
herteleo <leonard.hertel@gmail.com>
2019-04-23 21:46:39 +0200

    chore: update version in package.json on release

--- .releaserc.js
+++ .releaserc.js
@@ -6,6 +6,9 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    ['@semantic-release/npm', {
+      npmPublish: false,
+    }],
     '@semantic-release/git',
     '@semantic-release/gitlab',
   ],
