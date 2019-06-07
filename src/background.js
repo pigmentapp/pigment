f8b26c1b69a1d278f146492fd141a57d0cba3823
herteleo <leonard.hertel@gmail.com>
2019-06-07 18:53:20 +0200

    feat: switch between tabs with cmd/ctrl+[1-9]
    known issues: some apps will catch the shortcuts and will prevent the functionality

--- src/background.js
+++ src/background.js
@@ -1,4 +1,3 @@
-import electronLocalshortcut from 'electron-localshortcut';
 import windowStateKeeper from 'electron-window-state';
 import * as path from 'path';
 import { format as formatUrl } from 'url';
@@ -115,8 +114,6 @@ app.on('ready', async () => {
   }
   mainWindow = createMainWindow();
 
-  electronLocalshortcut.register('CmdOrCtrl+T', () => app.emit('app-router-goto-tabs-create'));
-
   Menu.setApplicationMenu(Menu.buildFromTemplate([
     {
       label: app.getName(),
@@ -132,16 +129,6 @@ app.on('ready', async () => {
       ],
     },
     {
-      label: 'File',
-      submenu: [
-        {
-          label: 'New Tab',
-          accelerator: 'CmdOrCtrl+N',
-          click: () => app.emit('app-router-goto-tabs-create'),
-        },
-      ],
-    },
-    {
       label: 'Edit',
       submenu: [
         { role: 'undo' },
@@ -152,6 +139,38 @@ app.on('ready', async () => {
         { role: 'paste' },
         { type: 'separator' },
         { role: 'selectAll' },
+      ],
+    },
+    {
+      label: 'Tabs',
+      submenu: [
+        { label: 'New Tab', accelerator: 'CmdOrCtrl+N', click: () => app.emit('app-router-goto-tabs-create') },
+        { type: 'separator' },
+        { label: 'Show tab 1', accelerator: 'CmdOrCtrl+1', click: () => app.emit('app-router-goto-tab-list-index', 1) },
+        { label: 'Show tab 2', accelerator: 'CmdOrCtrl+2', click: () => app.emit('app-router-goto-tab-list-index', 2) },
+        { label: 'Show tab 3', accelerator: 'CmdOrCtrl+3', click: () => app.emit('app-router-goto-tab-list-index', 3) },
+        { label: 'Show tab 4', accelerator: 'CmdOrCtrl+4', click: () => app.emit('app-router-goto-tab-list-index', 4) },
+        { label: 'Show tab 5', accelerator: 'CmdOrCtrl+5', click: () => app.emit('app-router-goto-tab-list-index', 5) },
+        { label: 'Show tab 6', accelerator: 'CmdOrCtrl+6', click: () => app.emit('app-router-goto-tab-list-index', 6) },
+        { label: 'Show tab 7', accelerator: 'CmdOrCtrl+7', click: () => app.emit('app-router-goto-tab-list-index', 7) },
+        { label: 'Show tab 8', accelerator: 'CmdOrCtrl+8', click: () => app.emit('app-router-goto-tab-list-index', 8) },
+        { label: 'Show tab 9', accelerator: 'CmdOrCtrl+9', click: () => app.emit('app-router-goto-tab-list-index', 9) },
+        {
+          label: 'Alternatives',
+          submenu: [
+            { label: 'New Tab', accelerator: 'CmdOrCtrl+T', click: () => app.emit('app-router-goto-tabs-create') },
+            { type: 'separator' },
+            { label: 'Show tab 1', accelerator: 'CmdOrCtrl+num1', click: () => app.emit('app-router-goto-tab-list-index', 1) },
+            { label: 'Show tab 2', accelerator: 'CmdOrCtrl+num2', click: () => app.emit('app-router-goto-tab-list-index', 2) },
+            { label: 'Show tab 3', accelerator: 'CmdOrCtrl+num3', click: () => app.emit('app-router-goto-tab-list-index', 3) },
+            { label: 'Show tab 4', accelerator: 'CmdOrCtrl+num4', click: () => app.emit('app-router-goto-tab-list-index', 4) },
+            { label: 'Show tab 5', accelerator: 'CmdOrCtrl+num5', click: () => app.emit('app-router-goto-tab-list-index', 5) },
+            { label: 'Show tab 6', accelerator: 'CmdOrCtrl+num6', click: () => app.emit('app-router-goto-tab-list-index', 6) },
+            { label: 'Show tab 7', accelerator: 'CmdOrCtrl+num7', click: () => app.emit('app-router-goto-tab-list-index', 7) },
+            { label: 'Show tab 8', accelerator: 'CmdOrCtrl+num8', click: () => app.emit('app-router-goto-tab-list-index', 8) },
+            { label: 'Show tab 9', accelerator: 'CmdOrCtrl+num9', click: () => app.emit('app-router-goto-tab-list-index', 9) },
+          ],
+        },
       ],
     },
   ]));
